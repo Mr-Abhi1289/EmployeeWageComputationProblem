9388fb6aeedb4f277cf3ea041a1b8d36e1f8d5fd
Mr-Abhi1289 <abhimvj575@gmail.com>
2022-11-23 11:26:43 +0530

    Added: Check Employee is Present or Absent

--- empWageCalculations.sh
+++ empWageCalculations.sh
@@ -1,1 +1,11 @@
 echo "Welcome to Employee Wage Computation Program"
+ispresent=1;
+randomcheck=$((RANDOM%2))
+
+if [ $randomcheck -eq $ispresent ]
+then
+	echo "employeee is present"
+else
+	echo "employee is absent"
+
+fi
